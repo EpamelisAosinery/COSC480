fdc842e92669c282ed6a6b8fc5853193f9e8c716
Jonathan <jtayl190@emich.edu>
2024-04-16 08:58:46 -0400

    comments

--- NETMonitoring/src/main/java/edu/emu/netmonitoring/controller/Panel2Controller.java
+++ NETMonitoring/src/main/java/edu/emu/netmonitoring/controller/Panel2Controller.java
@@ -8,19 +8,21 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.FileChooser;
-import org.pcap4j.core.*;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.util.Date;
 import java.util.Enumeration;
 
 public class Panel2Controller {
 
+    @FXML
+    private TableView<NetworkInterfaceInfo> tableView;
+    @FXML
+    private TableColumn<NetworkInterfaceInfo, String> cInterfaceName, cDisplayName, cIPAddress, cStatus, cIPType, cAction;
+
+    // There are javafx components in this file that are linked with an fxml file
+    // initialize is called to build / populate a table utilizing that layout
     public void initialize() {
         this.cInterfaceName.setCellValueFactory(new PropertyValueFactory<>("interfaceName"));
         this.cDisplayName.setCellValueFactory(new PropertyValueFactory<>("displayName"));
@@ -28,11 +30,15 @@ public class Panel2Controller {
         this.cStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
         this.cIPType.setCellValueFactory(new PropertyValueFactory<>("IPType"));
 
+        // to create a button for each row in the action column
         this.cAction.setCellFactory(col -> {
+            // Create a button to be used
             Button exportButton = new Button("Export");
+            // the cell we will be referencing
             TableCell<NetworkInterfaceInfo, String> cell = new TableCell<NetworkInterfaceInfo, String>() {
                 @Override
                 protected void updateItem(String item, boolean empty) {
+                    // here we say .... is the cell empty? if it is put a button in it
                     super.updateItem(item, empty);
                     if (empty) {
                         setGraphic(null);
@@ -41,7 +47,8 @@ public class Panel2Controller {
                     }
                 }
             };
-
+            // a simple event handler if the button is clicked
+            // perform method exportAction()
             exportButton.setOnAction(event -> {
                 NetworkInterfaceInfo info = cell.getTableRow().getItem();
                 if (info != null) {
@@ -51,15 +58,16 @@ public class Panel2Controller {
 
             return cell;
         });
-
+        // populate the table with the appropriate information
         this.tableView.setItems(this.getNetworkInterfaces());
     }
 
+    // the method called when export button is clicked. Needs to be implemented
     private void exportAction(NetworkInterfaceInfo info) {
         System.out.println("Button Pressed");
     }
 
-
+    // the core of the class. This is the Network Interface Information
     public static class NetworkInterfaceInfo {
 
         private String cInterfaceName;
@@ -90,6 +98,7 @@ public class Panel2Controller {
             return cStatus;
         }
 
+        // created a hack of how to tell if a network was ipv4 or ipv6
         public String getIPType() {
 
             if (getIpAddress().contains(":")) {
@@ -106,32 +115,34 @@ public class Panel2Controller {
 
     }
 
-    @FXML
-    private TableView<NetworkInterfaceInfo> tableView;
-    @FXML
-    private TableColumn<NetworkInterfaceInfo, String> cInterfaceName, cDisplayName, cIPAddress, cStatus, cIPType, cAction;
 
+    // really the whole thing. Collections the Information about all the network interfaces
     private ObservableList<NetworkInterfaceInfo> getNetworkInterfaces() {
-
+        // a place to store all the info
         ObservableList<NetworkInterfaceInfo> networkInterfaces = FXCollections.observableArrayList();
 
         try {
-
+            // try to run through all network interfaces on a local machine
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-
+            // loop through them... while theres another network interface
             while (interfaces.hasMoreElements()) {
+                // snag all of the addresses assigned to it
                 NetworkInterface individualInterface = interfaces.nextElement();
                 Enumeration<InetAddress> inetAddresses = individualInterface.getInetAddresses();
+                // while there are addresses to go through
                 while (inetAddresses.hasMoreElements()) {
+                    // add a new instance of that class to the list
+                    // snag its info
                     InetAddress theAddress = inetAddresses.nextElement();
                     networkInterfaces.add(new NetworkInterfaceInfo(individualInterface.getName(), individualInterface.getDisplayName(), theAddress.getHostAddress(), individualInterface.isUp() ? "Up" : "Down"));
                 }
             }
+            // its a try an catch ... it needs a catch
         } catch (SocketException e) {
             throw new RuntimeException(e);
         }
 
-
+        // return all the info
         return networkInterfaces;
     }
 
